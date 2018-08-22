84141706f54505e22be8e984b6028cc30270a9ed
WanBuJin <kanpass@163.com>
2018-08-22 21:14:24 +0800

    测试的sql语句修改

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -2,24 +2,32 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50621
+Source Server Version : 50723
 Source Host           : localhost:3306
-Source Database       : springboots
+Source Database       : test
 
 Target Server Type    : MYSQL
-Target Server Version : 50621
+Target Server Version : 50723
 File Encoding         : 65001
 
-Date: 2018-02-01 16:31:41
+Date: 2018-08-22 21:12:40
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for `users`
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `uname` varchar(20) DEFAULT NULL,
+  `usex` varchar(2) DEFAULT NULL,
+  `uphone` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('11', 'woshi', 'nv', '111111111');
